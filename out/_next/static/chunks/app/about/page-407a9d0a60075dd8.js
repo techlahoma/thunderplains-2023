7467654dbe379103df965f094888910cbd67a091
Vance Lucas <vance@vancelucas.com>
2023-04-27 18:19:43 -0500

    Deploy

---
+++ out/_next/static/chunks/app/about/page-407a9d0a60075dd8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{4349:function(n,e,u){Promise.resolve().then(u.t.bind(u,414,23))}},function(n){n.O(0,[414,667,139,744],function(){return n(n.s=4349)}),_N_E=n.O()}]);
