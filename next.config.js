b68fac909781407b399135f895b7f4f05c40e7b0
Vance Lucas <vance@vancelucas.com>
2023-04-27 18:18:11 -0500

    Next.js version + config update

--- next.config.js
+++ next.config.js
@@ -4,8 +4,9 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  //output: 'export',
+  output: 'export',
   basePath: '',
+  images: { unoptimized: true },
   experimental: {
     appDir: true,
   },
