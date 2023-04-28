2db6e953cc32b681428845990841b2968c36ee72
Vance Lucas <vance@vancelucas.com>
2023-04-28 12:35:32 -0500

    Update Sponsorship Prospectus

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,16 +3,16 @@ GIT_BRANCH=$(git symbolic-ref -q HEAD)
 GIT_REPO_URL=$(git config --get remote.origin.url)
 BUILD_FOLDER="out"
 
+# Build
+npm run build
+
 # Deploy & setup
 cd $BUILD_FOLDER
 rm .DS_Store
 git init .
 git remote add origin $GIT_REPO_URL
-git checkout gh-pages || (echo "Cannot chekout gh-pages branch!" && exit 1)
-git pull --rebase || (echo "Unable to pull remote changes on gh-pages branch!" && exit 1)
-
-# Build
-npm run build
+git checkout -b gh-pages || (echo "Cannot chekout gh-pages branch!" && exit 1)
+# git pull origin gh-pages --rebase || (echo "Unable to pull remote changes on gh-pages branch!" && exit 1)
 
 # Add CNAME file if present
 cp ../CNAME .
@@ -22,6 +22,6 @@ touch .nojekyll
 
 git add .
 git commit -am "Static site deploy"
-git push origin gh-pages
+git push origin gh-pages --force
 cd ..
 rm -rf $BUILD_FOLDER
