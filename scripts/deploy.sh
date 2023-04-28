640ba53cc4186d26e85465fd69052c7f83847fb4
Vance Lucas <vance@vancelucas.com>
2023-04-28 11:44:30 -0500

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,10 @@ cd $BUILD_FOLDER
 git init .
 git remote add origin $GIT_REPO_URL
 git checkout -b gh-pages
+
+# Add CNAME file if present
+cp ../CNAME .
+
 git add .
 git commit -am "Static site deploy"
 git push origin gh-pages --force
