95f6ab0d26619e3114ad462b9385611d4b9e7608
Vance Lucas <vance@vancelucas.com>
2023-04-28 12:05:59 -0500

    Static site deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,15 @@
 GIT_REPO_URL=$(git config --get remote.origin.url)
 BUILD_FOLDER="out"
 
-# Build
-npm run build
-
-# Deploy
+# Deploy & setup
 cd $BUILD_FOLDER
 git init .
 git remote add origin $GIT_REPO_URL
 git checkout -b gh-pages
+git pull origin gh-pages --rebase
+
+# Build
+npm run build
 
 # Add CNAME file if present
 cp ../CNAME .
@@ -19,6 +20,6 @@ touch .nojekyll
 
 git add .
 git commit -am "Static site deploy"
-git push origin gh-pages --force
+git push origin gh-pages
 cd ..
 rm -rf $BUILD_FOLDER
