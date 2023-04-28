696e368564734383c916897d1b8940a9a0428e85
Vance Lucas <vance@vancelucas.com>
2023-04-28 12:13:18 -0500

    Static site deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+GIT_BRANCH=$(git symbolic-ref -q HEAD)
 GIT_REPO_URL=$(git config --get remote.origin.url)
 BUILD_FOLDER="out"
 
@@ -7,8 +8,8 @@ cd $BUILD_FOLDER
 rm .DS_Store
 git init .
 git remote add origin $GIT_REPO_URL
-git checkout -b gh-pages
-git pull origin gh-pages --rebase
+git checkout -b gh-pages || (echo "Cannot chekout gh-pages branch!" && exit)
+git pull --rebase || (echo "Unable to pull remote changes on gh-pages branch!" && exit)
 
 # Build
 npm run build
